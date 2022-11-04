ea6ccd6a07b1816d7e02943a5de683c14619ea62
maximusbro <shidlovskiy.maksimka@gmail.com>
2022-11-04 23:43:31 +0100

    Basic is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string = "Dynamic title"
-  number: number = 53;
-  arr = [1,2,3];
-  obj = {a:1, b:{c:2}}
 
-  img ="https://www.patterns.dev/img/reactjs/react-logo@3x.svg"
-
-  inputValue: string = ""
-
-  constructor() {
-    setTimeout(() =>{
-      console.log("Timeout is over. Time switch from react to Angular!")
-      this.img ="https://angular.io/assets/images/logos/angular/angular.svg"
-    }, 3000 )
+  now: Date = new Date()
 
 
-  }
-  onClick() {
-      console.log("click")
-  }
-  onInput(event:KeyboardEvent){
-    console.log("Event", (<HTMLInputElement>event.target).value);
-    this.inputValue = (<HTMLInputElement>event.target).value;
-  }
-  onBlur(str: string) {
-  this.inputValue = str;
-  }
+
+
+
+
+
+
+
+
+
+  // arr = [1,1,2,3,5,8,13]
+ //
+ //    objs =  [
+ //            {
+ //    title: "Post1", author: "Maksym",
+ //  comments: [
+ //    {name:"Valera", text: "lorem2"},
+ //    {name:"Valera5", text: "lorem1"},
+ //    {name:"ValeraVasya", text: "lorem4"}
+ //  ]
+ //            },
+ //      {
+ //        title: "Post2", author: "Maksym2",
+ //        comments: [
+ //          {name:"ValeraSikin", text: "lorem2"},
+ //          {name:"Valera5", text: "lorem1"},
+ //          {name:"ValeraVasya", text: "lorem4"}
+ //        ]
+ //      }
+ //  ]
+
+
+  // toggle: any = false;
+
+  // title: string = "Dynamic title"
+  // number: number = 53;
+  // arr = [1,2,3];
+  // obj = {a:1, b:{c:2}}
+  //
+  // img ="https://www.patterns.dev/img/reactjs/react-logo@3x.svg"
+  //
+  // inputValue: string = ""
+  //
+  // constructor() {
+  //   setTimeout(() =>{
+  //     console.log("Timeout is over. Time switch from react to Angular!")
+  //     this.img ="https://angular.io/assets/images/logos/angular/angular.svg"
+  //   }, 3000 )
+  //
+  //
+  // }
+  // onClick() {
+  //     console.log("click")
+  // }
+  // onInput(event:KeyboardEvent){
+  //   console.log("Event", (<HTMLInputElement>event.target).value);
+  //   this.inputValue = (<HTMLInputElement>event.target).value;
+  // }
+  // onBlur(str: string) {
+  // this.inputValue = str;
+  // }
+
 }
