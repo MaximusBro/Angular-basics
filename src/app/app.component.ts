05e421193071dd7cbcb330cc7ef2cbcbe45010b3
maximusbro <shidlovskiy.maksimka@gmail.com>
2022-11-04 19:28:48 +0100

    First basics Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  title: string = "Dynamic title"
+  number: number = 53;
+  arr = [1,2,3];
+  obj = {a:1, b:{c:2}}
+
+  img ="https://www.patterns.dev/img/reactjs/react-logo@3x.svg"
+
+  inputValue: string = ""
+
+  constructor() {
+    setTimeout(() =>{
+      console.log("Timeout is over. Time switch from react to Angular!")
+      this.img ="https://angular.io/assets/images/logos/angular/angular.svg"
+    }, 3000 )
+
+
+  }
+  onClick() {
+      console.log("click")
+  }
+  onInput(event:KeyboardEvent){
+    console.log("Event", (<HTMLInputElement>event.target).value);
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+  onBlur(str: string) {
+  this.inputValue = str;
+  }
 }
