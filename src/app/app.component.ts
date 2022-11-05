9cdffff209e93b9d12c625c11a08dd1dc0c9a07d
maximusbro <shidlovskiy.maksimka@gmail.com>
2022-11-05 01:19:28 +0100

    Connections with Components (post -> app)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  now: Date = new Date()
-
-
-
-
-
-
-
-
-
-
-
-  // arr = [1,1,2,3,5,8,13]
- //
- //    objs =  [
- //            {
- //    title: "Post1", author: "Maksym",
- //  comments: [
- //    {name:"Valera", text: "lorem2"},
- //    {name:"Valera5", text: "lorem1"},
- //    {name:"ValeraVasya", text: "lorem4"}
- //  ]
- //            },
- //      {
- //        title: "Post2", author: "Maksym2",
- //        comments: [
- //          {name:"ValeraSikin", text: "lorem2"},
- //          {name:"Valera5", text: "lorem1"},
- //          {name:"ValeraVasya", text: "lorem4"}
- //        ]
- //      }
- //  ]
-
-
-  // toggle: any = false;
-
-  // title: string = "Dynamic title"
-  // number: number = 53;
-  // arr = [1,2,3];
-  // obj = {a:1, b:{c:2}}
-  //
-  // img ="https://www.patterns.dev/img/reactjs/react-logo@3x.svg"
-  //
-  // inputValue: string = ""
-  //
-  // constructor() {
-  //   setTimeout(() =>{
-  //     console.log("Timeout is over. Time switch from react to Angular!")
-  //     this.img ="https://angular.io/assets/images/logos/angular/angular.svg"
-  //   }, 3000 )
-  //
-  //
-  // }
-  // onClick() {
-  //     console.log("click")
-  // }
-  // onInput(event:KeyboardEvent){
-  //   console.log("Event", (<HTMLInputElement>event.target).value);
-  //   this.inputValue = (<HTMLInputElement>event.target).value;
-  // }
-  // onBlur(str: string) {
-  // this.inputValue = str;
-  // }
-
+  posts: Post[] = [
+    {title:"I want learn Angular components", text: "I still learning Components", id:1},
+    {title:"My opinion about Angular", text: "I think, i like Angular more, than React. He had all to development", id:2}
+  ]
+  updatePosts(post: Post ) {
+    this.posts.unshift(post)
+    // console.log('Post', post)
+  }
 }
